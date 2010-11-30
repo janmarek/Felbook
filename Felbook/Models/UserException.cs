d57d6051d210698061dbf3d8d8b38f406e2f3210
Jan Marek <mail@janmarek.net>
2010-11-30 17:29:13 +0100

    výjimka

---
+++ Felbook/Models/UserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Felbook.Models
+{
+	class UserException : Exception
+	{
+
+		public UserException(string message) : base(message)
+		{
+		
+		}
+	}
+}
