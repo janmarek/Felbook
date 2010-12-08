bfa3cba3583700c5ea196e426640bb068df8ec53
pistekjakub <pistejak@fel.cvut.cz>
2010-12-08 15:14:13 +0100

    * nyní že zobrazují error hlášky

--- Felbook/Controllers/ProfileController.cs
+++ Felbook/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Mvc;
 using Felbook.Models;
 using Felbook.Helpers;
+using System.Web;
+using System.IO;
+using System;
 
 #region view models
 namespace Felbook.Models
@@ -55,22 +58,73 @@ namespace Felbook.Controllers
 		}
 
 
+        [Authorize]
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(Felbook.Models.User model) //FormCollection collection
+        {
+            TryUpdateModel(CurrentUser);
 
-		[HttpPost, ValidateAntiForgeryToken, Authorize]
-		public ActionResult Edit(FormCollection collection)
-		{
-			TryUpdateModel(CurrentUser);
+            //upload změna profilového obrázku
+            Felbook.Helpers.ImageHelper imageOperator = new Felbook.Helpers.ImageHelper(); //pomocná třída pro operace s obrázky
+            HttpPostedFileBase imageToUpload = Request.Files["profileimage"];
+            int userId = CurrentUser.Id;
+            string fileDir = "../Web_Data/profile_images/";
+            //název souboru je vždy stejný
+            string fileName = "profileimage.png";
+            string fileFullPath = Path.Combine(HttpContext.Server.MapPath(fileDir + userId), fileName);
+            string fileDirPath = Path.GetDirectoryName(fileFullPath);
+            bool uploadImage = false;
 
-			if (ModelState.IsValid)
-			{
-				Model.UserService.Edit(CurrentUser);
-				return RedirectToAction("Index", new { username = CurrentUser.Username });
-			}
+            if (imageToUpload.ContentLength == 0)
+            {
+                uploadImage = false;
+            }
+            else if (Felbook.Helpers.ImageHelper.IsImage(imageToUpload.ContentType))
+            {
+                uploadImage = true;
+            }
+            else
+            {
+                ModelState.AddModelError("file", "Your file wasn't image.");
+            }
 
-			return View(CurrentUser);
-		}
+            if (model.OldPassword != null && String.Equals(model.Password, model.ConfirmPassword))
+            {
+                if (CurrentUser.CheckPassword(model.OldPassword))
+                {
+                    CurrentUser.ChangePassword(model.Password);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The password provided is incorrect.");
+                }
+            }
 
+            if (ModelState.IsValid)
+            {
+                //zpráva pro uživatele že editoval profil bez problému
+                ViewData["EditResult"] = "The edit is successful.";
+                
+                if (uploadImage == true)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(fileFullPath);
+                    }
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError("file", "Unexpected file error.");
+                        return View(CurrentUser);
+                    }
+                    imageOperator.ImageResize(imageToUpload, fileFullPath, 90, 120);
+                }
 
+                Model.UserService.Edit(CurrentUser);
+                return View(CurrentUser);
+            }
+            //v případě nějaké chyby se vrátí tohle
+            return View(CurrentUser);
+        }
 
 		[HttpPost, ValidateAntiForgeryToken, Authorize]
 		public ActionResult AddStatus(StatusFormModel formModel)
